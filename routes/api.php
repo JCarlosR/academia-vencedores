93702a5ebd72712db33b678e5b9ad39e65d7af2f
Juan Carlos <juancagb.17@gmail.com>
2017-04-19 18:04:54 -0500

    phpUnit tests

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Student;
+use Symfony\Component\HttpFoundation\Request;
+
+Route::get('/student', function (Request $request) {
+    return Student::where('dni', $request->input('dni'))->first();
+});
+
 Route::get('/students', 'Api\ReportController@students');
 Route::get('/courses', 'Api\ReportController@courses');
 Route::get('/levels', 'Api\ReportController@levels');
